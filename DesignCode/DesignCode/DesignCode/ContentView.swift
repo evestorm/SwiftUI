dc466a8eace1382de5bc6a0309b618052649ef18
evelance <1572534835@qq.com>
2020-07-21 00:11:04 +0800

    docs: 组件和视觉效果

--- DesignCode/DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/DesignCode/ContentView.swift
@@ -11,40 +11,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            VStack {
-                Text("Cark Back")
-            }
-            .frame(width: 300.0, height: 280.0)
-            .background(Color.blue)
-            .cornerRadius(10.0)
-            .shadow(radius: 20.0)
-            .offset(x: 0, y: -20.0)
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("accent"))
-                            .padding(.top)
-                        Text("Certificate")
-                            .foregroundColor(Color.white)
-                        
-                    }
-                    Spacer()
-                    Image("Logo1")
-                        .resizable()
-                        .frame(width: 30.0, height: 30.0)
-                }
-                .padding(.horizontal)
-                Spacer()
-                Image("Card1")
-            }
-            .frame(width: 340.0, height: 280.0)
-            .background(Color.black)
-            .cornerRadius(10.0)
-            .shadow(radius: 20)
+            TitleView()
+//                .blur(radius: 20)
+            
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 50), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+            CardView()
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10.0, y: 10.0, z: 10.0))
+            
+            CardBottomView()
         }
     }
 }
@@ -52,5 +40,83 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Cark Back")
+        }
+        .frame(width: 340.0, height: 280.0)
+        .background(Color.blue)
+        .cornerRadius(10.0)
+        .shadow(radius: 20.0)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("Certificate")
+                        .foregroundColor(Color.white)
+                    
+                }
+                Spacer()
+                Image("Logo1")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Card1")
+        }
+        .frame(width: 340.0, height: 280.0)
+        .background(Color.black)
+        .cornerRadius(10.0)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60.0, height: 6.0)
+                .cornerRadius(3.0)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .lineLimit(10)
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30.0)
+        .shadow(radius: 30)
+        .offset(x: 0, y: 500)
     }
 }
