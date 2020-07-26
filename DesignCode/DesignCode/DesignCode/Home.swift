59b56b344a1b95209eebec1754714619ada1bee4
evelance <1572534835@qq.com>
2020-07-26 15:56:59 +0800

    docs: SF符号和菜单创建

---
+++ DesignCode/DesignCode/DesignCode/Home.swift
@@ -0,0 +1,50 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by evestorm on 2020/7/26.
+//  Copyright © 2020 evelance. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            MenuRow(image: "person.crop.circle", text: "My Account")
+            MenuRow(image: "creditcard", text: "Billing")
+            MenuRow(image: "person.and.person", text: "Team")
+            MenuRow(image: "arrow.uturn.down", text: "Sign out")
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct MenuRow: View {
+    var image = "creditcard"
+    var text = "My Account"
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color("background2"))
+                .frame(width: 32, height: 32)
+            Text(text)
+                .font(.headline)
+            Spacer()
+        }
+    }
+}
---
+++ DesignCode/DesignCode/README.md
@@ -0,0 +1,8 @@
+#  Design Code
+
+## 快捷键
+
+
+- 开启关闭Xcode左侧边栏 `Cmd + 0`
+- 弹出视图选择菜单 `Shift + Cmd + L`
+
