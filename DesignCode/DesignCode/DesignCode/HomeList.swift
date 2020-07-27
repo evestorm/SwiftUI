38aed1f5d8aff650899df2ac47d38428a8967972
evelance <1572534835@qq.com>
2020-07-27 13:57:10 +0800

    docs: 模态弹窗

--- DesignCode/DesignCode/DesignCode/HomeList.swift
+++ DesignCode/DesignCode/DesignCode/HomeList.swift
@@ -9,13 +9,23 @@
 import SwiftUI
 
 struct HomeList: View {
+    @State var showCourseView = false
+    
+    var course = CourseData
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(0 ..< 3) { item in
-                    CourseView()
+            HStack(spacing: 30) {
+                ForEach(course) { item in
+                    Button(action: {
+                        self.showCourseView = true // 设置按钮点击动作
+                    }) {
+                        CourseView(title: item.title, image: item.image, color: item.color, shadowColor: item.shadowColor) // 把当前卡片设置为button的自定义外观
+                    }.sheet(isPresented: self.$showCourseView) {
+                        ContentView() // 弹出 contentView
+                    }
                 }
             }
+            .padding(.leading, 30)
             Spacer()
         }
     }
@@ -28,23 +38,54 @@ struct HomeList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+    var title = "Build an app with SwiftUI"
+    var image = "Card2"
+    var color = Color("darkblue")
+    var shadowColor = Color("darkblueshadow")
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Build an app with SwiftUI")
+            Text(title)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
                 .padding(20)
                 .lineLimit(4)
-                .frame(width: 150)
+                .padding(.trailing, 30)
             Spacer()
-            Image("Card2")
+            Image(image)
+                .renderingMode(.original)
                 .resizable()
+                .aspectRatio(contentMode: .fit)
                 .frame(width: 246)
+                .padding(.bottom, 30)
         }
-        .background(Color("darkblue"))
+        .background(color)
         .cornerRadius(30)
         .frame(width: 246, height: 360)
-        .shadow(color: Color("darkblueshadow"), radius: 20, x: 0, y: 20)
+        .shadow(color: shadowColor, radius: 20, x: 0, y: 20)
     }
 }
+
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var color: Color
+    var shadowColor: Color
+}
+
+let CourseData = [
+    Course(
+        title: "Build an app with SwiftUI",
+        image: "Card2",
+        color: Color("darkblue"),
+        shadowColor: Color("darkblueshadow")
+    ),
+    Course(
+        title: "Design Course",
+        image: "Card3",
+        color: Color("pink"),
+        shadowColor: Color("pinkshadow")
+    ),
+]
